d536e0ee37e077cac386eed64b708d389c0611ec
Chandan Saha <chandansaha1945@gmail.com>
2025-11-21 21:49:26 +0530

    c done

--- string/alternate.c
+++ string/alternate.c
@@ -1,3 +1,7 @@
+/**
+ * This C program takes a string as input, toggles the case of each word in the string, and outputs the
+ * modified string.
+ */
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
